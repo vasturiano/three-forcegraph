84372aa567fd3cf5e01c944264c65af88d1747d9
vasco <vastur@gmail.com>
2017-12-31 00:37:06 -0800

    Add three object kapsule wrapper

---
+++ src/index.js
@@ -0,0 +1,1 @@
+export { default } from "./three-forcegraph.js"
---
+++ src/three-forcegraph.js
@@ -0,0 +1,5 @@
+import { Group as ThreeGroup } from 'three';
+import ForceGraph from './forcegraph-kapsule.js';
+import fromKapsule from './kapsule-class.js';
+
+export default fromKapsule(ForceGraph, ThreeGroup, true);
