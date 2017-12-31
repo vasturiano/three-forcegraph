c4cf2413f7af8a2e8580ebd09e0e929a1a32e6e4
vasco <vastur@gmail.com>
2017-12-31 13:22:49 -0800

    Update indentation to 2sp

--- rollup.config.js
+++ rollup.config.js
@@ -4,21 +4,21 @@ import babel from 'rollup-plugin-babel';
 import { name, homepage, version } from './package.json';
 
 export default {
-    external: ['three'],
-    input: 'src/index.js',
-    output: [
-        {
-            format: 'umd',
-            name: 'ThreeForceGraph',
-            globals: { three: 'THREE' },
-            file: `dist/${name}.js`,
-            sourcemap: true,
-            banner: `// Version ${version} ${name} - ${homepage}`
-        }
-    ],
-    plugins: [
-        resolve(),
-        commonJs(),
-        babel({ exclude: 'node_modules/**' })
-    ]
+  external: ['three'],
+  input: 'src/index.js',
+  output: [
+    {
+      format: 'umd',
+      name: 'ThreeForceGraph',
+      globals: { three: 'THREE' },
+      file: `dist/${name}.js`,
+      sourcemap: true,
+      banner: `// Version ${version} ${name} - ${homepage}`
+    }
+  ],
+  plugins: [
+    resolve(),
+    commonJs(),
+    babel({ exclude: 'node_modules/**' })
+  ]
 };
--- src/color-utils.js
+++ src/color-utils.js
@@ -4,19 +4,19 @@ import tinyColor from 'tinycolor2';
 const colorStr2Hex = str => isNaN(str) ? parseInt(tinyColor(str).toHex(), 16) : str;
 
 function autoColorNodes(nodes, colorByAccessor, colorField) {
-    if (!colorByAccessor || typeof colorField !== 'string') return;
+  if (!colorByAccessor || typeof colorField !== 'string') return;
 
-    const colors = schemePaired; // Paired color set from color brewer
+  const colors = schemePaired; // Paired color set from color brewer
 
-    const uncoloredNodes = nodes.filter(node => !node[colorField]);
-    const nodeGroups = {};
+  const uncoloredNodes = nodes.filter(node => !node[colorField]);
+  const nodeGroups = {};
 
-    uncoloredNodes.forEach(node => { nodeGroups[colorByAccessor(node)] = null });
-    Object.keys(nodeGroups).forEach((group, idx) => { nodeGroups[group] = idx });
+  uncoloredNodes.forEach(node => { nodeGroups[colorByAccessor(node)] = null });
+  Object.keys(nodeGroups).forEach((group, idx) => { nodeGroups[group] = idx });
 
-    uncoloredNodes.forEach(node => {
-        node[colorField] = colors[nodeGroups[colorByAccessor(node)] % colors.length];
-    });
+  uncoloredNodes.forEach(node => {
+    node[colorField] = colors[nodeGroups[colorByAccessor(node)] % colors.length];
+  });
 }
 
 export { autoColorNodes, colorStr2Hex };
