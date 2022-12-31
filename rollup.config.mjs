147b346da505e5f70b367f2642e434686672f28a
vasco <vastur@gmail.com>
2022-12-31 01:12:44 +0000

    Bump dev deps

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonJs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 import dts from 'rollup-plugin-dts';
 import pkg from './package.json' assert { type: 'json' };
 
