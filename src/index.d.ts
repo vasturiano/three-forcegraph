b2c5b8f8d73d6e3d769ae4779e6c25ef9890b954
vasco <vastur@gmail.com>
2023-01-28 23:28:35 +0000

    Upd types

--- src/index.d.ts
+++ src/index.d.ts
@@ -39,7 +39,7 @@ interface ForceFn {
 
 type Coords = { x: number; y: number; z: number; }
 
-type LinkPositionUpdateFn = (obj: Object3D, coords: { start: Coords, end: Coords }, link: LinkObject) => null | boolean;
+type LinkPositionUpdateFn = (obj: Object3D, coords: { start: Coords, end: Coords }, link: LinkObject) => void | null | boolean;
 
 export declare class ThreeForceGraphGeneric<ChainableInstance> extends Object3D {
   constructor();
